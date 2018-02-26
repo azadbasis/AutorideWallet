4e7c5dff3cb0c1a69e77d8bbe988fc61a145af99
Ariful <smariful19@gmail.com>
2018-02-26 18:26:25 +0600

    login

---
+++ app/src/main/java/autoride/azhar/me/autoridewallet/CurrencyActivity.java
@@ -0,0 +1,14 @@
+
+package autoride.azhar.me.autoridewallet;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CurrencyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_currency);
+    }
+}
